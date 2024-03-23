fe87ccc12962e461c89bae96f534478913f7d749
Naima Hasan <naimahasan31@gmail.com>
2024-03-24 00:22:51 +0600

    Added common topbar

---
+++ src/components/recipe/RecipeDetails.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./RecipeDetails.css"
+
+export const RecipeDetails = () => {
+  return (
+    <div>
+        <div className="recipe-quote">
+        <h4>"Cappuccino, a classic Italian coffee creation, <br/>
+        Combines espresso, steamed milk, and frothy foam in harmonious relation. <br/>
+        Its rich flavor and creamy texture delight the senses with each sip's invitation."</h4>
+        </div>
+    </div>
+  )
+}
