9503ab1eb9e3326135e42621f2a39135abe25bbe
NaimaHasan <naimahasan31@gmail.com>
2024-03-24 16:36:50 +0600

    added a basic favorite screen

--- src/components/common/ItemCard.js
+++ src/components/common/ItemCard.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Heart, HeartFill } from "react-bootstrap-icons";
+import { Card } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import heroImage from "../../assets/homepage/hero.jpg";
+import "./ItemCard.css"
 
 export const ItemCard = () => {
+    const [isHoveredHeart, setIsHoveredHeart] = useState(false);
+    const [isHeartFilled, setIsHeartFilled] = useState(false);
+  
+    const handleHeartClick = () => {
+      setIsHeartFilled(!isHeartFilled);
+    };
+  
   return (
     <div>
-        dsaf
+        <Card className="item-card">
+            <Image src={heroImage} className="item-image" />
+            <div className="item-heart">
+              {isHeartFilled ? (
+                <HeartFill
+                  size="22px"
+                  className={isHeartFilled ? "icon-hover" : ""}
+                  style={{ color: "red" }}
+                  onClick={handleHeartClick}
+                />
+              ) : (
+                <Heart
+                  size="22px"
+                  className={isHoveredHeart ? "icon-hover" : ""}
+                  style={{ color: isHoveredHeart ? "red" : "black" }}
+                  onMouseEnter={() => setIsHoveredHeart(true)}
+                  onMouseLeave={() => setIsHoveredHeart(false)}
+                  onClick={handleHeartClick}
+                />
+              )}
+            </div>
+            <Card.Body className="item-name">
+              Cappucino
+            </Card.Body>
+          </Card>
     </div>
   )
 }
--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -3,10 +3,35 @@ import { TopBar } from '../components/common/TopBar'
 import { ItemCard } from '../components/common/ItemCard'
 
 export const Favorite = () => {
+  const notes =[1,1,1,1,1,1,1,1];
   return (
     <div style={{backgroundColor: " #e0d9c7",  minHeight: "200vh"}}>
       <TopBar/>
-      <ItemCard/>
+      <div>
+      {notes.length === 0 ? (
+        <div
+          style={{
+            fontSize: "25px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "50vh",
+          }}
+        >
+          <p>There is no note to display!</p>
+        </div>
+      ) : (
+        <div className="row" style={{padding: "200px"}}>
+          {notes.map((note, index) => (
+            <div key={index} className="col mb-4 d-flex justify-content-center" >
+              <ItemCard/>
+            </div>
+          ))}
+
+          <div className={`col-${12 - (notes.length % 4) * 3} mb-4`} />
+        </div>
+      )}
+    </div>
     </div>
   )
 }
